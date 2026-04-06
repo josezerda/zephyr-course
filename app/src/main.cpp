622131f298f452d34620c59921de9902451a1f28
Jose Zerda <jozerda@email.com>
2026-04-06 17:53:51 +0200

    l3-task1: Update

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,11 +1,18 @@
 #include <zephyr/kernel.h>
+#include <zephyr/drivers/gpio.h>
+
+/* Onboard LED on NUCLEO-H7S3L8 */
+#define LED0_NODE DT_ALIAS(led0)
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 
 int main(void)
 {
-#ifdef CONFIG_LED_SUBSYSTEM
+    gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+
     while (1) {
+        gpio_pin_toggle_dt(&led);
         k_msleep(CONFIG_LED_BLINK_SLEEP_MS);
     }
-#endif
+
     return 0;
 }
